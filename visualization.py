2846fe98b7db5395fb2463f6736580fdb51196d7
Avi-1029 <ava.xz80@gmail.com>
2025-04-04 18:58:52 +0400

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -24,3 +24,43 @@ plt.plot(x1,y1,'y-.',linewidth = 1,markersize = 6, label = "x² = y")
 plt.plot(x1,y2,'r-.',linewidth = 1,markersize = 6, label = "x³ = y")
 plt.legend()
 plt.show()
+
+# #bar plots:
+# x = [1, 3, 5, 7, 9]
+# y = [9, 4, 6, 8, 3]
+# plt.bar(x,y)
+# plt.show()
+
+# #barchart with categorical x values:
+# x = ["Europe", "Canada", "India", "USA", "China"]
+# y = [9, 4, 6, 8, 3]
+# plt.bar(x,y)
+# plt.show()
+
+# #Multiple Bar Graphs:
+# x = [1,3,5,7,9]
+# x1 = [2,4,6,8,10]
+# y = [9, 4, 6, 8, 3]
+# y1 = [6,5,9,7,10]
+# plt.bar(x1,y,color = "pink")
+# plt.bar(x,y1,color = "blue")
+# plt.xticks(ticks = x, labels = ["Europe" , "Canada", "India", "USA", "China"])
+# plt.show()
+
+#Histrograms:
+bob = np.random.randint(20, 60, 100)
+daniel = np.arange(20, 60, 5)
+plt.hist(bob, daniel, edgecolor = "darkblue")
+plt.show()
+
+#ScatterPlot:
+x = [4,5,7,10,11]
+y = [2,1,7,6,12]
+plt.scatter(x,y,marker = "*", s = 20, color = "darkred")
+plt.show()
+
+#PieChart:
+slices = [4,5,7,10,11]
+labels = ["bananas","strawberry","dragonfruit","papaya","pear"]
+plt.pie(slices, labels = labels,autopct='%1.0f%%')
+plt.show()
