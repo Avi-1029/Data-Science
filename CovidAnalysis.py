bef6eb6b67329193e3f7ebf50896efc6aaa04e08
Avi-1029 <ava.xz80@gmail.com>
2025-06-27 19:16:52 +0400

    Update CovidAnalysis.py

--- CovidAnalysis.py
+++ CovidAnalysis.py
@@ -22,3 +22,22 @@ x = ttc.index
 y = ttc['Confirmed']
 plt.scatter(x, y, marker = "*", s = 20, color = "darkred")
 plt.show()
+
+#Bar plot for top ten countries with highest deaths:
+
+kevin = harry.sort_values("Deaths", ascending = False).head(10)
+x = kevin.index
+y = kevin['Deaths']
+plt.bar(x, y, 0.9, edgecolor = "black")
+plt.show()
+
+#Country:
+
+print(covid['Country_Region'].value_counts())
+spain = covid[covid['Country_Region'] == "Spain"]
+
+top10spain = spain.sort_values("Confirmed", ascending = False).head(10)
+x = top10spain['Province_State']
+y = top10spain['Confirmed']
+plt.bar(x, y, 0.9 , edgecolor = 'black')
+plt.show()
